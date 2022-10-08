eb1db180c20504d35aa3c41cd2540f55ef7bb678
0xlaine <94804332+0xlaine@users.noreply.github.com>
2022-10-08 10:46:26 +0200

    Complete the draw code w. drand round `2327000`

--- src/draw.mjs
+++ src/draw.mjs
@@ -1,5 +1,8 @@
+import chalk from 'chalk';
 import {getRound} from "./rand/drand.mjs";
 import {createRng} from "./rand/rng.mjs";
+
+const ROUND_TO_USE = 2327000;
 
 export async function draw(options) {
     if(!options.eligibleFile) {
@@ -8,12 +11,32 @@ export async function draw(options) {
 
     const {
         randomness
-    } = await getRound(2324512);
+    } = await getRound(ROUND_TO_USE);
 
     if(!randomness) {
         throw new Error('Failed to fetch round from drand');
 
     }
     const rng = createRng(randomness);
-    console.log('** NOT IMPLEMENTED YET**');
+
+    const eligible = JSON.parse(fs.readFileSync(options.eligibleFile));
+
+    const eligibleWithLuckyNumber = eligible.map(item => ({
+        delegator: item.delegator,
+        amount: item.amount,
+        luckyNumber: rng()
+    })).sort((a, b) => {
+        if (a.luckyNumber < b.luckyNumber) return 1;
+        return -1;
+    });
+
+    console.log(eligibleWithLuckyNumber);
+    let nr = 0;
+    for (const wallet of eligibleWithLuckyNumber) {
+        console.log(`#${chalk.green(nr)}: ${chalk.cyan(wallet.delegator)} w. lucky number: ${chalk.yellow(wallet.luckyNumber.toFixed(5))} (staked amount: ${wallet.amount.toFixed(4)})`)
+        nr++;
+        if(nr === 5) {
+            console.log('///-------------------------- winners ABOVE this line --------------------------///')
+        }
+    }
 }
